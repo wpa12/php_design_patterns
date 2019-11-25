8ac7846ea09bacc404b7ceb2fd88791c2a631686
Wayne <wpa12@live.com>
2019-11-25 09:46:16 +0000

    Update singleton.php
    added pdo

--- singleton.php
+++ singleton.php
@@ -6,6 +6,12 @@ class Database {
 
 	private function __construct () {
 		//set to private so only methods inside the class can access it.
+		
+		$this->dsn = 'mysql:dbname=testdb;host=127.0.0.1';
+		$this->username = 'default';
+		$this->password = 'default';
+		
+		$this->conn = new PDO($this->dsn, $this->username, $this->password);
 	}
 
 	public static function getInstance () {
